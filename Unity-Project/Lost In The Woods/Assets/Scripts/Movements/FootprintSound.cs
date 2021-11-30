7b7e9ae5adb67b02893845255552fce1c662bc71
LukasTiefenthaler <lukas.tiefenthaler1@gmail.com>
2021-11-30 18:59:13 +0100

    Fixed Run + Dirt Footstep + office fix
    Fixed Office Wall and added Collider for Chairs Table and drawers.
    Also changed position of Table and Chairs that play can walk behind table

--- Unity-Project/Lost In The Woods/Assets/Scripts/Movements/FootprintSound.cs
+++ Unity-Project/Lost In The Woods/Assets/Scripts/Movements/FootprintSound.cs
@@ -6,19 +6,32 @@ public class FootprintSound : MonoBehaviour
 {
     [SerializeField] private AudioSource soundSource;
     [SerializeField] private AudioClip[] footprintSound;
-    CharacterController cc;
+    [SerializeField] private float footstepDuration = 1;
 
     void Start()
     {
-        cc = GetComponent<CharacterController>();
+        StartCoroutine(Step());
     }
 
     void Update()
     {
-        if((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !soundSource.isPlaying)
+        //if((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !soundSource.isPlaying)
+        //{
+        //    soundSource.clip = footprintSound[Random.Range(0, footprintSound.Length)];
+        //    soundSource.Play(0);
+        //}
+    }
+
+    IEnumerator Step()
+    {
+        while (true)
         {
-            soundSource.clip = footprintSound[Random.Range(0, footprintSound.Length)];
-            soundSource.Play(0);
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                soundSource.PlayOneShot(footprintSound[Random.Range(0, footprintSound.Length)]);
+                yield return new WaitForSeconds(footstepDuration);
+            }
+            yield return null;
         }
     }
 }
